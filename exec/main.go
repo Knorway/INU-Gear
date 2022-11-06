152f133132af554347a9d3cc3444088c7412c25a
knorway <rlaxogud921@gmail.com>
2022-11-07 00:58:12 +0900

    feat: add go binary executable for end users

---
+++ exec/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func GetLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
+func main() {
+
+	fmt.Printf("[[ --> MOBILE URL: %s:%s <-- ]]\n", GetLocalIP(), "3000")
+
+	dir := strings.Split(os.Args[0], "/up")[0]
+
+	cmd := exec.Command("bash", "-c", "docker-compose up --build")
+	os.Chdir(dir)
+
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	cmd2 := exec.Command("echo", "y", "|", "docker image prune")
+	os.Chdir(dir)
+
+	cmd2.Stdout = os.Stdout
+	err = cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+
+}
